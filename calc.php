e9abee96ef6b885252a604ae2a3aea96b7a315dd
Nafiyad Menberu <68880766+NafMKD@users.noreply.github.com>
2020-09-05 07:03:38 +0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -105,9 +105,7 @@ echo '
 <div class="box-header with-border">
   <h3 class="box-title">Grade Result</h3>
   <div class="box-tools pull-right">
-        <button type="button" class="btn btn-box-tool" data-widget="collapse">
-		<i class="fa fa-minus"></i>
-  	</button>
+        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
   </div>
 </div>
 
